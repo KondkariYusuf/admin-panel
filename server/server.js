bb7013afbd76fb284f35d962b37b814927e492af
KondkariYusuf <kondkariyusuf345@gmail.com>
2025-11-27 19:22:21 +0530

    portfolio integrated with frontend, cloudinary remaining

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const cors = require('cors');
 
 connectDB();
 
-const app = express();
 
+const app = express();
+app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
